d7bc1a609fad541cf7ef12ea7c78c1984e1d8794
abhishekgupta79 <abhigupta1004@gmai.com>
2025-06-01 11:20:27 +0530

    adding comments

--- abhi.py
+++ abhi.py
@@ -1,32 +1,37 @@
-print("hellloo world ")
 
+# Print hello world 
+print("hellloo world ")
+# print hello with user name
 name = input("enter yuor name")
 print("hello," + name + "!")
 
+# AGE calculator
 Birth_year = int(input("Enter youur birth year"))
 current_year = 2025
 age = current_year - Birth_year
-print("you are", age, "year old.")"""
-"""
+print("you are", age, "year old.")
 
+# finding odd and even number
 num = int(input("Enter a number"))
 if num % 2 == 0 :
     print("even")
 else:
     print("odd")
 
-
+#simple calculator
 a = float(input("Enter first number :")) 
 b = float(input("Enter second number :"))
 print("sum:", a + b )
 print("subtraction:", a - b )
 print("multiplicatin:", a * b )
 
+# print 1 to 10
 i = 1
 while i <= 10:
     print(i)
     i += 1 
 
+# Password checker 
 correct_password = "abhishek gupta"
 entered_password = input("enter your password")
 if entered_password == correct_password:
@@ -34,18 +39,15 @@ if entered_password == correct_password:
 else:
     print("access enabled") 
 
-
+#celcicus to fehrenheit
 celcius = float(input("enter temperature in celcius"))
 fahrenheit = (celcius * 9/5) + 32
 print("temprature in fahrenheit", fahrenheit)
 
-
+#print fruits help of liist and loop
 
 fruits = [" apple\n banana\n mango"]
 for fruit in fruits:
     print (fruit)
 
-a = float(input("enter a first number"))
-b = float(input("enter a second"))
-multiplicatin = a*b
-print(multiplicatin)
+
